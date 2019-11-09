66f7c5dd17b611cbf42dbe088a08760a84d4bbe5
ahmadhmirza <ahmadhasanmirza@gmail.com>
2019-11-09 09:32:13 +0100

    Generated apk v1.0

--- src/android_foo/android_test/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ src/android_foo/android_test/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -81,12 +81,12 @@ public final class R {
         public static final int packed = 0x7f080092;
         public static final int parent = 0x7f0800aa;
         public static final int percent = 0x7f0800ad;
-        public static final int right = 0x7f0800b8;
-        public static final int spread = 0x7f0800df;
-        public static final int spread_inside = 0x7f0800e0;
-        public static final int start = 0x7f0800e5;
-        public static final int top = 0x7f080110;
-        public static final int wrap = 0x7f08013c;
+        public static final int right = 0x7f0800b7;
+        public static final int spread = 0x7f0800de;
+        public static final int spread_inside = 0x7f0800df;
+        public static final int start = 0x7f0800e4;
+        public static final int top = 0x7f08010f;
+        public static final int wrap = 0x7f08013b;
     }
     public static final class styleable {
         private styleable() {}
